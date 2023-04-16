ed8c55397153326115b9c77393836a22c2cd6e3a
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-16 15:40:14 -0600

    re-create-recipe-item-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
     RecipesComponent,
     RecipesListComponent,
     RecipesDetailComponent,
+    RecipesItemComponent,
   ],
   imports: [
     BrowserModule
