a581462d19584d0e8baa25e958dc17d44886097e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-18 19:41:21 -0600

    declare-the-recipes-list-array

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
+
+  recipes = [];
 
 }
