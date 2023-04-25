9982307bda489a293942f8d321c11d6d7056f79f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-25 13:31:53 -0600

    Set-up-logic-for-nfiF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-and-shopping-list-app';
-  menuButtonValueOption:string ;
+  menuButtonValue:string = 'Recipes' ;
 }
