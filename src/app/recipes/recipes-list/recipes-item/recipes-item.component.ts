4b225f278a9dfb8f2e6a05f57951934393cad8dc
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-27 12:15:31 -0600

    add-input-value

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent {
-
+  @Input('childRecipe') recipe ;
 }
