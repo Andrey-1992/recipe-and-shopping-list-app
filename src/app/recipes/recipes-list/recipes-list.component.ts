7fc5a43de13da47cd54510f5795f618824ff8987
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-27 12:24:57 -0600

    Activate-html-data

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe','Simple Test','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg'),
-    new Recipe('A test recipe','Simple Test','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg')
+    new Recipe('A test recipe 1','Simple Test 1','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg'),
+    new Recipe('A test recipe 2','Simple Test 2','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg')
   ];
 
   // test0 = this.recipes[0].name
