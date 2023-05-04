a0913e2b4265db9cc76cce8966de678d2301499c
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-04 14:15:43 -0600

    add-hostListener

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 @Directive({
   selector:'[appDropdown]'
 })
 
 export class DropdownDirective {
+  
   constructor(private elementRef: ElementRef, private render: Renderer2) {}
   
   ngOnInit() {
     this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
+  }
+  
+  @HostListener() mouseover() {
+
   }
 
 }
