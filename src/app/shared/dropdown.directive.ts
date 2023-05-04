a048093196bf1e922a136db4c2fc84bc40935044
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-04 14:10:24 -0600

    add-render-property-to-directive-function

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,13 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 @Directive({
   selector:'[appDropdown]'
 })
 
 export class DropdownDirective {
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private render: Renderer2) {}
   
   ngOnInit() {
-    this.elementRef.nativeElement.style.backgroundColor = "blue";
+    this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
   }
 
 }
