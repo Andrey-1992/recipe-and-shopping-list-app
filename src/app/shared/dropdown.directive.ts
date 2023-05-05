6f84630106088f5cbf079fd44dd023d1747aaa6e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-05 12:06:44 -0600

    create-host-listener

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -10,25 +10,30 @@ import { Directive,
 })
 
 export class DropdownDirective {
-  @Input() defaultColor: string = 'transparent';
-  @Input() highlightColor: string = 'blue';
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-  
-  constructor(private elementRef: ElementRef, private render: Renderer2) {}
-  
-  ngOnInit() {
-    // this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
+  isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
   }
+  // @Input() defaultColor: string = 'transparent';
+  // @Input() highlightColor: string = 'blue';
+  // @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  
+  // constructor(private elementRef: ElementRef, private render: Renderer2) {}
+  
+  // ngOnInit() {
+  //   // this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
+  // }
 
-  @HostListener('mouseenter') mouseover(eventData: Event) {
-    // this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
-    this.backgroundColor = this.highlightColor;
-  } 
+  // @HostListener('mouseenter') mouseover(eventData: Event) {
+  //   // this.render.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
+  //   this.backgroundColor = this.highlightColor;
+  // } 
 
-  @HostListener('mouseleave') mouseleave(eventData: Event) {
-    // this.render.setStyl`e(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
-    this.backgroundColor = this.defaultColor;
-  } 
+  // @HostListener('mouseleave') mouseleave(eventData: Event) {
+  //   // this.render.setStyl`e(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+  //   this.backgroundColor = this.defaultColor;
+  // } 
 
 
   // @HostListener('onClick') onClick(eventData: Event) {
