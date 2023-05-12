4990cd170310794995ab87d51b9789884eccaf96
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-12 17:19:28 -0600

    add-event-emitter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,7 +14,11 @@ export class ShoppingListComponent implements OnInit{
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    // console.log(this.ingredients)
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
   
 }
