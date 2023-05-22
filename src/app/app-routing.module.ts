fbe4d0d005e318df9a72449de58b3b98ae13afea
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-22 11:37:23 -0600

    add-defaul-0start-compo entto-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch:'full'},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: 'recipes', component: RecipesComponent, children: [
+    {path: '', component: RecipeStartComponent},
     {path: ':id', component: RecipesDetailComponent}
   ]},
 ];
