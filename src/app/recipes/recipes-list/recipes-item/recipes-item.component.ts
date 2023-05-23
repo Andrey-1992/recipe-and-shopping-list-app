f42c673ff998c032f708f719a7f5b5a9466913ec
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-23 14:13:15 -0600

    remove-unused-code-in-detail-component

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+// import { Router, ActivatedRoute } from '@angular/router';
+// import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipes-item',
@@ -13,14 +13,10 @@ export class RecipesItemComponent {
   @Input('childRecipe') recipe: Recipe;
   @Input('childRecipeNum') recipeNum: number;
 
-  constructor(private recipeService: RecipeService,
-      private router: Router,
-      private activatedRoute: ActivatedRoute
-    ) {}
-
-  ngOnInit() {
-    // console.log(this.recipeNum)
-  }
+  // constructor(private recipeService: RecipeService,
+  //     private router: Router,
+  //     private activatedRoute: ActivatedRoute
+  //   ) {}
   
   // onSelected() {
   //   this.recipeService.recipeSelected.emit(this.recipe);
