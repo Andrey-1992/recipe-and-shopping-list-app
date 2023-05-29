33f5cbe3156568ed21337c0953968f16c4a0ac85
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-05-29 12:58:17 -0600

    craete-subscription-fior-eklemetns

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.modle';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -9,6 +10,7 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit{
   ingredients:Ingredient[]
+  private igChangeSub: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
