d193168ddc77dbeaa650b7752f7a66ccc62f9c80
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-06-06 19:54:42 -0600

    create-subject-for-selected-ingredients-service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import {  Subject} from 'rxjs';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   ingredients:Ingredient[] = [
     new Ingredient("Apples 🍎 ", 5),
