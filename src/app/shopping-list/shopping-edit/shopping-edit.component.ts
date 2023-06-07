5260cc9120312c6c0ef4658aa7f2823e81ab53ac
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-06-07 14:32:07 -0600

    set-up-logic-to-update-ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,13 +35,14 @@ export class ShoppingEditComponent implements OnInit{
       );
   }
   
-  onAddItem() {
-    event.preventDefault()
-    // console.log(this.ingredientsForm)
-    this.shoppingListService.addIngredient({
-      name: this.ingredientsForm.controls.ingredientName.value,
-      amount: this.ingredientsForm.controls.ingredientAmount.value,
-    });
+  onAddItem(f) {
+    const value = f.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   clearForm() {
