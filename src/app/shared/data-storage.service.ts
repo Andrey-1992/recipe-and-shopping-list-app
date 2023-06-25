c1db4b0edb6c73a5ee7ff25b3dfbbf71eb495169
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-06-25 15:23:05 -0600

    add-map-operator-to-fetch-servoce

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -15,7 +16,9 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://udemy-recipe-course-f2d4f-default-rtdb.firebaseio.com/recipes.json')
-    .pipe(map())
+    .pipe(map(recipes =. {
+      
+    }))
     .subscribe(response => { this.recipeService.setRecipes(response) })
   }
 };
