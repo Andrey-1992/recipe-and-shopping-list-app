7787962a9e55189eefd1ebc30868786c13f9406d
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-06 17:29:11 -0600

    import-resolve-librartry

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core'
+import { Resolve } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
-export class RecipeResolverService {}
+export class RecipeResolverService implements Resolve {}
