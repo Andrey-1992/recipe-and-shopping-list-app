bf3c18fe0dd44b18a5e9e860e3cfd8e3843371ba
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-06 17:30:08 -0600

    import-recipe-model

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Resolve } from '@angular/router';
+import { Recipe } from './recipe.model';
 
 @Injectable({providedIn: 'root'})
-export class RecipeResolverService implements Resolve {}
+export class RecipeResolverService implements Resolve<Recipe[]> {}
