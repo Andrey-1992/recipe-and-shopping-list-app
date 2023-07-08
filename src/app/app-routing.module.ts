992a3f55bb298c7617994fe8d6072a6924f8925b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-08 13:06:07 -0600

    add-routing-for-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./recipes/recipe-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
 
 
 const appRoutes: Routes = [
@@ -15,7 +16,8 @@ const appRoutes: Routes = [
     {path: '', component: RecipeStartComponent},
     {path: 'new', component: RecipeEditComponent},
     {path: ':id', component: RecipesDetailComponent, resolve: [RecipeResolverService]},
-    {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService]}
+    {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService]},
+    {path: 'auth', component: AuthComponent},
   ]},
 ];
 @NgModule({
