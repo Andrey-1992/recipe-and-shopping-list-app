f9adb6a64bcd7f1922cba9a30ffe2db718276198
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-08 14:28:35 -0600

    set-up-=logic-to-handle-sing-up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -23,6 +23,20 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+    
     form.reset();
   }
 
