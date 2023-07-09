e0ae50779d344f0daf63b22be492551eb5487e78
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-09 16:20:11 -0600

    add-loading-spinner-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner-component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,6 +33,7 @@ import { AuthComponent } from './auth/auth.component';
     RecipeStartComponent,
     AuthComponent,
     RecipeEditComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
