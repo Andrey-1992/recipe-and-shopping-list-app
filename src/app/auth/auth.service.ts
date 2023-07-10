552cbb9f3bc643316c99cec0c43cf46f34afa4b9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-10 17:40:23 -0600

    reuse-logic-for-singup-and-login-sections

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -30,9 +30,7 @@ export class AuthService {
       }
     )
     .pipe(catchError(this.handleError), tap(resData => {
-      const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-      const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
-      this.user.next(user);
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
     }))
   }
 
@@ -44,7 +42,9 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+    }));
   }
 
   private handleAuthentication(email:string, userId: string, token: string, expiresIn: number) {
