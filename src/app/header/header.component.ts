214a0a25bd51fc2e9e26b8b7ff2d7c9a2953aeff
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-10 17:53:51 -0600

    create-ob ject-to-subscribe-and-unsubscribe

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
@@ -7,19 +8,27 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
   collapsed = true;
+  private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService, authService: AuthService) {}
-
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe();
+  }
+  
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
-
+  
   onFetchData() {
     this.dataStorageService.fetchRecipes();
   }
-
+  
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
+  }
   // @Output('internalFeatureSelected') featureSelected = new EventEmitter<string>();
   // onSelect(feature) {
   //   // console.log(feature.srcElement.innerText)
