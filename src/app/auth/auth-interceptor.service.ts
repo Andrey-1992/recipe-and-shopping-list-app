f540f7e89247ce4ec6d2ead49e8cbad199a71119
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-11 15:43:53 -0600

    add-interceptor-logic-block

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,17 +1,22 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor{
   constructor(private authService: AuthService) {};
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    this.authService.user.
-
-    return next.handle(req);
+    return this.authService.user.pipe(
+      take(1), 
+      exhaustMap(user => {
+        const modifiedReq = req.clone({
+          params: new HttpParams().set('auth', user.token)
+      });
+        return next.handle(modifiedReq);
+      }));
   }
-
 
 };
