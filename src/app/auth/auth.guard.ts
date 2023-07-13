e1c4a48afce3607456f6541cec1bd9366d022b67
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-13 11:49:48 -0600

    Add-can-actrivate-route-to-functionality

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -4,13 +4,13 @@ import { map, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 
-@Injectable({providedIn:'root'});
+@Injectable({providedIn:'root'})
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     return this.authService.user.pipe(map(user => {
       return !!user;
-    }))
+    }));
   }
 }
