4123781ecb293cdd89d7ef05b51112ffb4c7631e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-07-13 12:00:30 -0600

    add-take-operator

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { AuthService } from './auth.service';
 
 
@@ -9,7 +9,9 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return this.authService.user.pipe(map(user => {
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
       const isAuth = !!user;
       if (isAuth) {
         return true;
